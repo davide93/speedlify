ca1a88b7a3bd4519c9a95b864a4cffed2d1d0b4e
Davide Piccagnoni <davide@retiche.it>
2022-03-08 16:22:42 +0100

    Update sample.js

--- _data/sites/sample.js
+++ _data/sites/sample.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: "Sample", // optional, falls back to object key
 	description: "The default sites that get tested",
 	options: {
-		frequency: 60 * 1, // (in minutes), 23 hours
+		frequency: 60 * 23, // (in minutes), 23 hours
 	},
 	urls: [
 		"https://retiche.it"
